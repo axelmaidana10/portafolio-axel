5e407057bfa4340c0fef0d8be759f12f3c0c1638
axelmaidana10 <axelestudio2214@gmail.com>
2024-10-15 07:58:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("contactBtn").addEventListener("click", function() {
+    const contactInfo = document.getElementById("contactInfo");
+    contactInfo.classList.toggle("hidden");
+});
